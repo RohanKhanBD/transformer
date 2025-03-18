0e3ba0be494df0e6a68100e90fbf7684e2a913d9
Rohan Khan <rk8399165@gmail.com>
2025-03-18 11:48:59 +0600

    Fix typo precision.

--- train.py
+++ train.py
@@ -161,6 +161,6 @@ def main(fabric: Fabric):
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
     logger = TensorBoardLogger("runs")
-    fabric = Fabric(prcision="bf16-mixed", loggers=[logger])
+    fabric = Fabric(precision="bf16-mixed", loggers=[logger])
     fabric.launch()
     main(fabric)
