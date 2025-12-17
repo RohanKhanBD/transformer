6463c9c236c7b4b38c1a78aaf1f91d04a68c16e2
Rohan Khan <rk8399165+github@gmail.com>
2025-12-17 21:27:37 +0600

    rename things and making module error.

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
-from pre_process import pre_process
-from iterdataset import TextDataset
+from .preprocess import pre_process
+from .iterdataset import TextDataset
 
 __all__ = ["pre_process", "TextDataset"]
