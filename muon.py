b5bac94f1b025638474302f9b33c87f84b79fdce
Rohan Khan <rk8399165+github@gmail.com>
2026-01-03 20:45:27 +0600

    using the express.

--- muon.py
+++ muon.py
@@ -11,16 +11,15 @@ coeffs_list = [
 ]
 
 
-def zeropower_via_newtonschulz5(G: torch.Tensor, steps: int):
+def zeropower_via_newtonschulz5(G: torch.Tensor):
     assert G.ndim >= 2
-    a, b, c = (3.4445, -4.7750, 2.0315)
 
     X = G.bfloat16()
     if G.size(-2) >= G.size(-1):
         X = X.mT
 
     X = X / (X.norm(dim=(-2, -1), keepdim=True) * (1 + 2e-2) + 1e-6)
-    for _ in range(steps):
+    for a, b, c in coeffs_list:
         A = X @ X.mT
         B = b * A + c * A @ A
         X = a * X + B @ X
@@ -30,14 +29,12 @@ def zeropower_via_newtonschulz5(G: torch.Tensor, steps: int):
     return X
 
 
-def muon_update(
-    grad: torch.Tensor, momentum: torch.Tensor, beta=0.95, ns_steps=5, nesterov=True
-):
+def muon_update(grad: torch.Tensor, momentum: torch.Tensor, beta=0.95, nesterov=True):
     momentum.lerp_(grad, 1 - beta)
     update = grad.lerp_(momentum, beta) if nesterov else momentum
     if update.ndim == 4:
         update = update.view(len(update), -1)
-    update = zeropower_via_newtonschulz5(update, ns_steps)
+    update = zeropower_via_newtonschulz5(update)
     update *= max(1, grad.size(-2) / grad.size(-1)) ** 0.5
     return update
 
@@ -49,14 +46,12 @@ class Muon(optim.Optimizer):
         lr=0.02,
         weight_decay=0.1,
         momentum=0.95,
-        ns_steps: int = 5,
         nesterov: bool = True,
     ):
         defaults = dict(
             lr=lr,
             weight_decay=weight_decay,
             momentum=momentum,
-            ns_steps=ns_steps,
             nesterov=nesterov,
         )
         super().__init__(params, defaults)
